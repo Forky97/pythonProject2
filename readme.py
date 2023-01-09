7be01a754b04a2f748ca46c1456ac86b55e9b46e
Cristian <cristis991@mail.ru>
2023-01-09 10:55:29 +0200

    класс привет

--- readme.py
+++ readme.py
@@ -1,1 +1,11 @@
-print('привет поставь мне 1')
+class Git_start :
+
+    def __init__(self,name=None):
+        self.name = name
+
+    def privet(self):
+        print(self.name)
+
+
+cris = Git_start('cris')
+cris.privet()
